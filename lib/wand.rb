47d507b3e3a6d81760db4b8c7a1ed2d4cd6d91a7
John Nunemaker <nunemaker@gmail.com>
2010-03-06 01:00:58 -0500

    Using backticks instead of %x.

--- lib/wand.rb
+++ lib/wand.rb
@@ -3,7 +3,7 @@ require 'mime/types'
 module Wand
   def self.wave(path)
     type = MIME::Types.type_for(path)[0].to_s
-    type = %x[#{executable} --mime --brief #{path}].split(';')[0] if type.nil? || type == ''
+    type = `#{executable} --mime --brief #{path}`.split(';')[0] if type.nil? || type == ''
     type = nil if type =~ /cannot\sopen/
     type
   end
